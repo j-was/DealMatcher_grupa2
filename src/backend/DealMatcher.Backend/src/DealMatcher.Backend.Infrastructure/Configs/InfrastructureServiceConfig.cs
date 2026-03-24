47878dc623e8ee7cbd22407952352bd9c49675bc
Jerzy Wąsiewicz <01189175@pw.edu.pl>
2026-03-24 17:47:13 +0100

    docker compose fixed

--- src/backend/DealMatcher.Backend/src/DealMatcher.Backend.Infrastructure/Configs/InfrastructureServiceConfig.cs
+++ src/backend/DealMatcher.Backend/src/DealMatcher.Backend.Infrastructure/Configs/InfrastructureServiceConfig.cs
@@ -9,7 +9,7 @@ public static class InfrastructureServiceConfig
       ConfigurationManager config,
       ILogger logger)
     {
-        var connectionString = config.GetConnectionString("SqliteConnection");
+        var connectionString = config.GetConnectionString("DefaultConnection");
         Guard.Against.Null(connectionString);
         services.AddApplicationDbContext(connectionString);
         services.AddAutoMapperConfigs();

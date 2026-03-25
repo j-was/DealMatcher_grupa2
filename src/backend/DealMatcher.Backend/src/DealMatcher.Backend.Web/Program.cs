59b11074fdb2f7778980edfbc84f9356181ab364
Jerzy Wąsiewicz <01189175@pw.edu.pl>
2026-03-25 11:55:39 +0100

    cors policy call adjusted

--- src/backend/DealMatcher.Backend/src/DealMatcher.Backend.Web/Program.cs
+++ src/backend/DealMatcher.Backend/src/DealMatcher.Backend.Web/Program.cs
@@ -56,6 +56,8 @@ public sealed class Program
 
             var app = builder.Build();
 
+            app.UseCors("AllowFrontend");
+
             await app.UseAppMiddlewareAndSeedDatabase();
 
             await app.RunAsync();

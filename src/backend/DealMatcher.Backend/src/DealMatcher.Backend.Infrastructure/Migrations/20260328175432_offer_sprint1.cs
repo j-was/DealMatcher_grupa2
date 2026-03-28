ab9cae92d63e970ca1417a01ba977b26241dacf1
Jerzy Wąsiewicz <01189175@pw.edu.pl>
2026-03-28 18:55:36 +0100

    service migration

---
+++ src/backend/DealMatcher.Backend/src/DealMatcher.Backend.Infrastructure/Migrations/20260328175432_offer_sprint1.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DealMatcher.Backend.Infrastructure.Migrations;
+
+/// <inheritdoc />
+public partial class offer_sprint1 : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+
+    }
+}

7d21d63a35280820c0e00e895da74e48d580d59e
Ondrey <andrzejwrzesinski.2004@gmail.com>
2026-04-07 23:16:02 +0200

    Format fixes 2

--- src/backend/DealMatcher.Backend/src/DealMatcher.Backend.UseCases/Features/User/Get/GetUserQuery.cs
+++ src/backend/DealMatcher.Backend/src/DealMatcher.Backend.UseCases/Features/User/Get/GetUserQuery.cs
@@ -1,3 +1,3 @@
 namespace DealMatcher.Backend.UseCases.Features.User.Get;
 
-public sealed record GetUserQuery(int userId): IRequest<Result<UserDTO>>;
+public sealed record GetUserQuery(int userId) : IRequest<Result<UserDTO>>;

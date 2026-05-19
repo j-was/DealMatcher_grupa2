aea648760929b9a2d3df63ee61de11fd50997db4
Ondrey <andrzejwrzesinski.2004@gmail.com>
2026-05-20 01:25:18 +0200

    Format change v5

--- src/backend/DealMatcher.Backend/src/DealMatcher.Backend.Web/Endpoints/Ban/Delete.cs
+++ src/backend/DealMatcher.Backend/src/DealMatcher.Backend.Web/Endpoints/Ban/Delete.cs
@@ -1,4 +1,4 @@
-using DealMatcher.Backend.UseCases.Features.Admin.DeleteBan;
+using DealMatcher.Backend.UseCases.Features.Ban.DeleteBan;
 
 namespace DealMatcher.Backend.Web.Endpoints.Ban;
 
--- src/backend/DealMatcher.Backend/src/DealMatcher.Backend.Web/Endpoints/Ban/GetBan.cs
+++ src/backend/DealMatcher.Backend/src/DealMatcher.Backend.Web/Endpoints/Ban/GetBan.cs
@@ -1,4 +1,4 @@
-using DealMatcher.Backend.UseCases.Features.Admin.GetBanById;
+using DealMatcher.Backend.UseCases.Features.Ban.GetBanById;
 
 namespace DealMatcher.Backend.Web.Endpoints.Ban;
 

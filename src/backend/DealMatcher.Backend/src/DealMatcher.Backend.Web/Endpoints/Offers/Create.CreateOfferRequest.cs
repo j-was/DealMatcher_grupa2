ddf0e5bf21686466b0af48a8e070347f3ac6f9f1
Ondrey <andrzejwrzesinski.2004@gmail.com>
2026-06-10 00:12:25 +0200

    format changes

--- src/backend/DealMatcher.Backend/src/DealMatcher.Backend.Web/Endpoints/Offers/Create.CreateOfferRequest.cs
+++ src/backend/DealMatcher.Backend/src/DealMatcher.Backend.Web/Endpoints/Offers/Create.CreateOfferRequest.cs
@@ -1,12 +1,12 @@
 namespace DealMatcher.Backend.Web.Endpoints.Offers;
 
- public sealed record CreateOfferRequest(
-      string Title,
-      string Description,
-      double Price,
-      string Tags,
-      int CategoryId,
-      string Properties,
-      int Availability,
-      List<IFormFile> Images
-  );
+public sealed record CreateOfferRequest(
+   string Title,
+   string Description,
+   double Price,
+   string Tags,
+   int CategoryId,
+   string Properties,
+   int Availability,
+   List<IFormFile> Images
+ );
